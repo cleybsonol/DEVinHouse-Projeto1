4bb3f1aed0c7b3f15efb73bd84b1daf6a7ff9dfe
cleybsonol <cleybson.reginaldo@gmail.com>
2020-12-03 19:09:41 -0300

    Commit Inicial
    Commit Base

---
+++ js/localStorage.js
@@ -0,0 +1,32 @@
+// Pega tudo do localStorage
+getFromLocalStorage();
+
+// Função para adicionar os TO-DOs no localstorage
+function addToLocalStorage(todos) {
+    // conver the array to string then store it.
+    localStorage.setItem('todos', JSON.stringify(todos));
+    // render them to screen
+    loadTodos(todos);
+  }
+  
+
+// Função para buscar os TO-DOs do localStorage
+  function getFromLocalStorage() {
+    const check = localStorage.getItem('todos');
+    // checar se existe itens no local storage
+    if (check) {
+      // Converte para array e guarda todos
+      todos = JSON.parse(check);
+      loadTodos(todos);
+    }
+  }
+
+// Função para limpar os TO-DOs no localstorage
+function clearLocalStorage(todos) {
+  // limparo localstorage
+  localStorage.clear();
+  // zera o array   
+  todos = [];
+  // renderizar para a tela
+  loadTodos(todos) 
+}
---
+++ js/principal.js
@@ -0,0 +1,155 @@
+// Construção dos elementos FORM,INPUT e ITEMS
+// Selecionar o todo-form
+const todoForm = document.querySelector('.todo-form');
+// Selecionar o input do form
+const todoInput = document.querySelector('.todo-input');
+// Selecionar o <ul> com a class="todo-tasks"
+const todoTasksList = document.querySelector('.todo-tasks');
+// Selecionar o elemento para notificar o erro de input <p>
+const link = document.querySelector('.alert');
+// SweetAlert
+/* const Swal = require('sweetalert2'); */
+
+// Array para armazenar os TO-DOs
+var todos = [];
+
+// Adicionar o eventListener no form, e escutar o evento de submit.
+todoForm.addEventListener('submit', function (event) {
+    // Previnir o reload da pagina
+    event.preventDefault();
+    if (checkExist(todos)) {
+        Swal.fire("Valor já existente");
+        link.innerHTML = "Atividade já existe na lista!";
+    }
+    else if (todoInput.value == '') {
+        link.innerHTML = "Informe uma atividade valida!"
+    }
+    else {
+        link.innerHTML = ""
+        addTodo(todoInput.value); // chama a função addTodo(adicionar) com o input que está no form
+    }
+});
+
+function checkExist(todos) {
+    /* Procura no array (SOME) o valor do input e retorna true caso existe valor igual. */
+    /* Passei os campos para Lower case para validar de forma case insensitive*/
+    return todos.some(x => x.name.toLowerCase() === todoInput.value.toLowerCase())
+};
+
+/* Funcão criada para testar a mesma solução de checkexist utilizando o FIND
+function checkExist(todos){
+    todos.find(function (task) {
+    /* Procura no array(FIND) o valor do input e retorna true caso existe valor igual */
+/*      if (task.name.toLowerCase() === todoInput.value.toLowerCase()) {
+          console.log("existe");
+          return;   
+      }
+      else {
+          return;
+      }
+})};*/
+
+
+// Função para adicionar TO-DOs
+function addTodo(task) {
+    // Se a task não é vazia.
+    if (task !== '') {
+        // Cria o objeto, com id, nome e propriedade completed 
+        const todo = {
+            id: Date.now(),
+            name: task,
+            completed: false
+        };
+        // adiciona o objeto no array dos TODOs
+        todos.push(todo);
+        addToLocalStorage(todos); // chamar a função para guardar no localstorage
+        todoInput.value = ''; //Limpar o valor do input//
+    }
+}
+
+// Função para CARREGAR na tela os TO-Dos
+function loadTodos(todos) {
+    // Limpa a <ul> das tasks com class=todo-tasks
+    todoTasksList.innerHTML = '';
+
+    // Laço foreach para passar atráves de cada task dentro do array
+    todos.forEach(function (task) {
+        // Checa se a task está completa
+        const flagged = task.completed ? 'checked' : null;
+
+        // cria um elemento <li> e o popula
+        // <li> </li>
+        const li = document.createElement('li');
+        // <li class="task"> </li>
+        li.setAttribute('class', 'task');
+        // <li class="task" data-key="%dataatual%"> </li>
+        li.setAttribute('data-key', task.id);
+        /* Exemplo: <li class="task" data-key="%dataatual%"> 
+              <input type="checkbox" class="checkbox">
+              tarefa a criar
+              <button class="delete-button">X</button>
+            </li> */
+        // Se as tarefas estão completas, então adiciona a classe ao <li> chamada 'flagged' e adiciona o CSS determinado.
+        if (task.completed === true) {
+            li.classList.add('checked');
+        }
+        li.innerHTML = `
+      <input type="checkbox" class="checkbox" ${flagged}>${task.name}<button class="delete-button">Remover</button>
+    `;
+        // adiciona o <li> ao <ul>
+        todoTasksList.append(li);
+    });
+
+}
+
+// Sinalizar completo ou não completo
+function flagging(id) {
+    todos.forEach(function (task) {
+        if (task.id == id) {
+            task.completed = !task.completed;
+        }
+    });
+    addToLocalStorage(todos);
+}
+
+// deleta o Todo (tarefa/task) para o ARRAY, para atualizar o localstorage e renderizar a atualização na tela
+function deleteTodo(id) {
+    todos = todos.filter(function (task) {
+        // usar != not !==, são tipagem diferente. número x string
+        return task.id != id;
+    });
+    // Atualizar o localStorage
+    addToLocalStorage(todos);
+}
+
+// Escutar os eventos de click no checkbox e delete.
+todoTasksList.addEventListener('click', function (event) {
+    // verifica se o evento foi no checkbox
+    if (event.target.type === 'checkbox') {
+        flagging(event.target.parentElement.getAttribute('data-key'));
+    }
+
+    // verifica se o evento botão Delete.
+    if (event.target.classList.contains('delete-button')) {
+       // If Confirm utilizando o ALERTA normal, desabilitado para utilizaro sweetalert
+        /*  if (confirm('Deseja realmente excluir?') == true) {
+            deleteTodo(event.target.parentElement.getAttribute('data-key')); */
+         swal.fire({
+            icon: 'warning',
+            title: 'Você está certo disso?',
+            text: 'Ao confirmar o item será removido da lista!',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            //removi a colocação do botão cancelar, tornando o alerta sobre a confirmação/* cancelButtonColor: '#d33', */
+            confirmButtonText: 'Sim, tenho certeza!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {if (result.value) {
+            deleteTodo(event.target.parentElement.getAttribute('data-key'));  
+            //Retirei o mensagem de remoção com sucesso para economizar click do usuário
+            /*swal.fire('Ação executada','O item foi removido da lista!','success'); */
+            }
+        })
+
+      }
+    }
+);
